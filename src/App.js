844e64fc39e8dec9a44d73560262284e7657ca23
unknown <jeffreyrnelson@outlook.com>
2020-11-29 23:00:54 -0500

    helium integraton attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("Button Pushed");
+    this.getDataAxios();
+  }
+
+  async getDataAxios() {
+    const response = await axios.post(
+      "https://console.helium.com/api/v1/down/d9804ab8-4597-4120-93ce-604e06e2b011/mH5J_JuND0P4M_vUTVyYfz4X11v0JiOz",
+      {
+        payload_raw: "SGVsbG8sIHdvcmxkIQ==",
+        port: 1,
+        confirmed: false,
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+    console.log(response.data);
+  }
+
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.handleClick}>
+          Click Me
+        </button>
+        <p>{this.state.username}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
